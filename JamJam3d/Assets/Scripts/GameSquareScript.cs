f66bf113088e64fbf31eef3c69e730c7dfae30e1
billwpierce <wwpierce020602@gmail.com>
2017-12-21 22:55:57 -0800

    added example clickable quad

---
+++ JamJam3d/Assets/Scripts/GameSquareScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSquareScript : MonoBehaviour {
+
+	public void OnMouseDown(){
+		Debug.Log ("clicked");
+	}
+
+}
