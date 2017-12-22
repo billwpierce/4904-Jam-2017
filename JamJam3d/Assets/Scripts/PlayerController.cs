5eab29ec06205e732905bac9cf9f6233c83f1a05
billwpierce <wwpierce020602@gmail.com>
2017-12-21 16:22:35 -0800

    gameboard generation

--- JamJam3d/Assets/Scripts/PlayerController.cs
+++ JamJam3d/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,22 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
+	public GameObject wheat;
+	public GameObject coal;
+	public GameObject wood;
+
+	public int[,] board = new int[16, 16];
+	public GameObject[,] clones = new GameObject[16,16];
+
+	void Start()
+	{
+		for (int i = 0; i < board.GetLength(0); i++) {
+			for (int j = 0; j < board.GetLength(1); j++) {
+				board[i,j] = Random.Range(0, 3);
+			}
+		}
+	}
+
 	void Update()
 	{
 		if (!isLocalPlayer)
@@ -17,10 +33,29 @@ public class PlayerController : NetworkBehaviour {
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
 
+		this.renderLocalBoard(this.board);
+
 	}
 	public override void OnStartLocalPlayer()
 	{
 		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+	public void renderLocalBoard(int[,] board)
+	{
+		for (int i = 0; i < board.GetLength(0); i++) {
+			for (int j = 0; j < board.GetLength(1); j++) {
+				int xpos = i - (board.GetLength (0) / 2);
+				int ypos = j - (board.GetLength (1) / 2);
+				Destroy (clones [i, j]);
+				if (board[i,j] == 0) {
+					clones[i,j] = (GameObject) Instantiate (wood, new Vector3 (xpos, 0, ypos), new Quaternion(0,0,0,0));
+				}else if (board[i,j] == 1) {
+					clones[i,j] = (GameObject) Instantiate (coal, new Vector3 (xpos, 0, ypos), new Quaternion(0,0,0,0));
+				}else if (board[i,j] == 2) {
+					clones[i,j] = (GameObject) Instantiate (wheat, new Vector3 (xpos, 0, ypos), new Quaternion(0,0,0,0));
+				}
+			}
+		}
 	}
 }
 
