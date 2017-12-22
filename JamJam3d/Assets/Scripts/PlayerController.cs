cbf077c3f94ee6b0d6abd73dc280593592c09c7a
billwpierce <wwpierce020602@gmail.com>
2017-12-21 22:08:47 -0800

    fixed board destroying

--- JamJam3d/Assets/Scripts/PlayerController.cs
+++ JamJam3d/Assets/Scripts/PlayerController.cs
@@ -33,7 +33,10 @@ public class PlayerController : NetworkBehaviour {
 	public override void OnStartLocalPlayer()
 	{
 		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
 
+	void OnDisable(){
+		destroyLocalBoard ();
 	}
 
 	public void makeLocalBoard(){
@@ -50,7 +53,6 @@ public class PlayerController : NetworkBehaviour {
 			for (int j = 0; j < board.GetLength(1); j++) {
 				int xpos = i - (board.GetLength (0) / 2);
 				int ypos = j - (board.GetLength (1) / 2);
-				Destroy (clones [i, j]);
 				if (board[i,j] == 0) {
 					clones[i,j] = (GameObject) Instantiate (wood, new Vector3 (xpos, 0, ypos), new Quaternion(0,0,0,0));
 				}else if (board[i,j] == 1) {
@@ -58,6 +60,14 @@ public class PlayerController : NetworkBehaviour {
 				}else if (board[i,j] == 2) {
 					clones[i,j] = (GameObject) Instantiate (wheat, new Vector3 (xpos, 0, ypos), new Quaternion(0,0,0,0));
 				}
+			}
+		}
+	}
+
+	public void destroyLocalBoard(){
+		for (int i = 0; i < board.GetLength (0); i++) {
+			for (int j = 0; j < board.GetLength (1); j++) {
+				Destroy (clones [i, j]);
 			}
 		}
 	}
